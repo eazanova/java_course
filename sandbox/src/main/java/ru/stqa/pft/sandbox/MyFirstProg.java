fb8ef6060c7f93d435baed1f13a0c166285f5563
eazanova <eazanova86@gmail.com>
2016-04-09 17:11:34 +0500

    Изменение настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProg {
 
-    public static void main(String[] args) {
-    	System.out.println("Hello, world");
-    }
-    
+public class MyFirstProg {
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
+
 }
