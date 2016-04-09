d090065895b8607e46899458b6a7a2c7f9ed7920
eazanova <eazanova86@gmail.com>
2016-04-10 02:28:32 +0500

    Добавили новый функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -1,9 +1,33 @@
 package ru.stqa.pft.sandbox;
 
-
 public class MyFirstProg {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    Hello("wold");
+    Hello("user");
+
+    double l = 5;
+    System.out.println("Площадь квадрата " + l + " = " + area(l));
+
+    double a = 6;
+    double b = 6;
+    System.out.println("Площадь квадрата = " + area(a, b));
+
+/*
+    double l = 5;
+    double s = l*l;
+    System.out.println("Площадь квадрата " + l + " = " + s);*/
+  }
+
+  public static void Hello(String somebody) {
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
