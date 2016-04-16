38adb8cdd8b8a218f1ab493132e2d2bd5c18620b
eazanova <eazanova86@gmail.com>
2016-04-17 00:18:44 +0500

    очистка лишнего

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -13,23 +13,6 @@ public class Point {
     this.y=y;
   }
 
-    /*
-    // Метод вычисляющий расстояние между двумя точками
-    double getDistance(Point a) {
-      return Math.sqrt( Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2) );
-
-
-      http://kostin.ws/java/example-class-point.html
-      http://info.javarush.ru/JavaRush_tasks_discussion/2013/12/02/level06-lesson08-task03.html
-    }*/
-
-/*
-   class Point {
-      public static double distance(int x1, int x2, int y1, int y2) {
-        double x = (x2-x1)^2 + (y2-y1)^2;
-        return Math.sqrt(x);
-    }
-*/
   }
 
 
