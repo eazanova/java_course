060d664daafafffa2162c9e429e64262c9e99880
eazanova <eazanova86@gmail.com>
2016-05-02 23:28:01 +0500

    делегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.AplicationManager;
+
+/**
+ * Created by Евгения on 02.05.2016.
+ */
+public class TestBase {
+
+  protected final AplicationManager app = new AplicationManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    //wd.quit();
+    app.stop();
+  }
+
+}
