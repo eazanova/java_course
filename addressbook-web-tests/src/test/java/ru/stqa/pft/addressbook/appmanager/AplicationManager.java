c8018b9c6008beeb0463b2ecf3bcd382f3519074
eazanova <eazanova86@gmail.com>
2016-05-10 22:46:35 +0500

    Запуск тестов под разными браузерами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,15 +12,29 @@ import java.util.concurrent.TimeUnit;
  * Created by Евгения on 02.05.2016.
  */
 public class AplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private NavigationHalper navigationHalper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper; //новый
+  private String browser;
 
-    public void init() {
-    wd = new FirefoxDriver();
+  public AplicationManager(String browser) {
+    this.browser = browser;
+  }
+
+  public void init() {
+     // String browser = BrowserType.FIREFOX;
+      if (browser == BrowserType.FIREFOX) {
+        wd = new FirefoxDriver();
+      } else if (browser == BrowserType.CHROME) {
+        wd = new ChromeDriver();
+      } else if (browser == BrowserType.IE) {
+        wd = new InternetExplorerDriver();
+      }
+
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     //groupHelper.wd.get("http://localhost/addressbook/"); у меня его почему-то нет
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.AplicationManager;
@@ -9,7 +10,7 @@ import ru.stqa.pft.addressbook.appmanager.AplicationManager;
  */
 public class TestBase {
 
-  protected final AplicationManager app = new AplicationManager();
+  protected final AplicationManager app = new AplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
