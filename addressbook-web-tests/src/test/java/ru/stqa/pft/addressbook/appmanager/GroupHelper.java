1eee0269f371685b95fa5a5aede073fe7b8201a5
eazanova <eazanova86@gmail.com>
2016-05-31 00:15:14 +0500

    урок 4 (4,5,6)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,12 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Евгения on 02.05.2016.
@@ -39,8 +43,9 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup() {
-    click(By.name("selected[]"));
+  public void selectGroup(int index) {
+    wd.findElements(By.name("selected[]")).get(index).click();
+   // click(By.name("selected[]"));
   }
 
   public void initGroupModification() {
@@ -64,5 +69,16 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+  return groups;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -4,19 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDelitionTests extends  TestBase {
 
     @Test
     public void testGroupDelition() {
       app.getNavigationHalper().gotoGroupPage();
-      int before = app.getGroupHelper().getGroupCount();
+
       if (! app.getGroupHelper().isThereAgroup()){
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
       }
-      app.getGroupHelper().selectGroup();
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+      //int before = app.getGroupHelper().getGroupCount();
+      app.getGroupHelper().selectGroup(before.size() - 1);
       app.getGroupHelper().deleteSelectedGroups();
       app.getGroupHelper().returnToGroupPage();
-      int after = app.getGroupHelper().getGroupCount();
-      Assert.assertEquals(after, before - 1);
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+      //int after = app.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after.size(), before.size() - 1);
+
+      before.remove(before.size() - 1);
+      // for (int i = 0; i <after.size(); i++) { //assertEquals сам умеет цикл
+        Assert.assertEquals(before, after);
+
     }
 }
