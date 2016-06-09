ebca606339566ed2169015bd6d4e3ad2723f4369
eazanova <eazanova86@gmail.com>
2016-06-10 00:19:45 +0500

    задание 10_2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDelitionTests extends TestBase {
 
@@ -9,11 +12,17 @@ public class ContactDelitionTests extends TestBase {
     public void testContactDelition() {
         app.getcontactHelper().returntoHome();
         if (! app.getcontactHelper().isThereAContact()){
-            app.getcontactHelper().createContact(new ContactData("test", "test", "test", "test", "test.test@test.ru", "test1"), true);
+            app.getcontactHelper().createContact(new ContactData("test", "test", "test", "test", "test.test@test.ru", "test1"));
         }
-        app.getcontactHelper().selectContact();
+        List<ContactData> before = app.getcontactHelper().getContactList();
+       // app.getcontactHelper().selectContact();
+        app.getcontactHelper().selectContact(before.size() - 1);
         app.getcontactHelper().initContactDeletion();
         app.getcontactHelper().submitContactDeletion();
         app.getcontactHelper().returntoHome();
+        List<ContactData> after = app.getcontactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 }
